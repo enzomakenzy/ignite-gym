d6dd783f511ed98e60b6532bf25b566e16af953b
enzomakenzy <enzomqueiroz@gmail.com>
2025-06-13 14:37:44 -0300

    Finalização da interface da tela de login

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,8 +3,9 @@ import { TextInputProps } from "react-native";
 
 export function Input({ ...rest }: TextInputProps) {
   return (
-    <GluestackInput className="bg-colorsTheme-gray-700 h-14 w-full text-mdTheme mb-4 rounded-md">
-      <InputField { ...rest } className="text-colorsTheme-white w-full px-4 placeholder:text-colorsTheme-gray-300 rounded-md focus:border focus:border-colorsTheme-green-500" />
+    <GluestackInput className="bg-colorsTheme-gray-700 h-14 w-full text-mdTheme mb-4 rounded-md border-0">
+      <InputField { ...rest } 
+      className="text-colorsTheme-white w-full px-4 placeholder:text-colorsTheme-gray-300 rounded-md focus:border focus:border-colorsTheme-green-500" />
     </GluestackInput>
   );
 }
